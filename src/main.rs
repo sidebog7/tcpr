1f9ae93be1cfc762de2c24a69b9b111f85654120
Gareth Pendleton <gareth.sidebottom@gmail.com>
2019-03-17 19:23:02 +0000

    Main sets up a connection

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+extern crate tun_tap;
+use std::io;
+
+fn main() -> io::Result<()> {
+    let nic = tun_tap::Iface::new("tun0",tun_tap::Mode::Tun)?;
+    let mut buf = [0u8; 1504];
+    let nbytes = nic.recv(&mut buf[..])?;
+    eprintln!("Read {} bytes: {:x?}", nbytes, &buf[..nbytes]);
+    Ok(())
 }
