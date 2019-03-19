cba504468e39b2303f7643034241060c79ea980e
Gareth Pendleton <gareth.sidebottom@gmail.com>
2019-03-19 16:26:42 +0000

    Loop and output flags and proto

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,20 @@ extern crate tun_tap;
 use std::io;
 
 fn main() -> io::Result<()> {
-    let nic = tun_tap::Iface::new("tun0",tun_tap::Mode::Tun)?;
+    let nic = tun_tap::Iface::new("tun0", tun_tap::Mode::Tun)?;
     let mut buf = [0u8; 1504];
-    let nbytes = nic.recv(&mut buf[..])?;
-    eprintln!("Read {} bytes: {:x?}", nbytes, &buf[..nbytes]);
+    loop {
+        let nbytes = nic.recv(&mut buf[..])?;
+        let flags = u16::from_be_bytes([buf[0], buf[1]]);
+        let proto = u16::from_be_bytes([buf[2], buf[3]]);
+
+        eprintln!(
+            "Read {} bytes (flags: {:x}, proto: {:x}): {:x?}",
+            nbytes - 4,
+            flags,
+            proto,
+            &buf[4..nbytes]
+        );
+    }
     Ok(())
 }
