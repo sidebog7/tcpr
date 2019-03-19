ab076ebbb72b5e427ae3ba51a08a7d58052154b6
Gareth Pendleton <gareth.sidebottom@gmail.com>
2019-03-19 16:26:20 +0000

    Add Ctrl+C break to shell script

--- run.sh
+++ run.sh
@@ -1,8 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 cargo b --release
 sudo setcap CAP_NET_ADMIN=eip ./target/release/tcpr
 ./target/release/tcpr &
 pid=$!
 sudo ip addr add 192.168.0.1/24 dev tun0
 sudo ip link set up dev tun0
+trap "kill $pid" INT TERM
 wait $pid
