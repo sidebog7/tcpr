c2aef0514402dbd7797a2276627c41c1e7ff3e39
Gareth Pendleton <gareth.sidebottom@gmail.com>
2019-03-21 15:39:01 +0000

    Parsed the IPV4 and the TCP packet

--- src/main.rs
+++ src/main.rs
@@ -6,16 +6,41 @@ fn main() -> io::Result<()> {
     let mut buf = [0u8; 1504];
     loop {
         let nbytes = nic.recv(&mut buf[..])?;
-        let flags = u16::from_be_bytes([buf[0], buf[1]]);
-        let proto = u16::from_be_bytes([buf[2], buf[3]]);
+        let _eth_flags = u16::from_be_bytes([buf[0], buf[1]]);
+        let eth_proto = u16::from_be_bytes([buf[2], buf[3]]);
+        if eth_proto != 0x0800 {
+            // Ignore non IPV4
+            continue;
+        }
 
-        eprintln!(
-            "Read {} bytes (flags: {:x}, proto: {:x}): {:x?}",
-            nbytes - 4,
-            flags,
-            proto,
-            &buf[4..nbytes]
-        );
+        match etherparse::Ipv4HeaderSlice::from_slice(&buf[4..nbytes]) {
+            Ok(p) => {
+                let src = p.source_addr();
+                let dst = p.destination_addr();
+                let proto = p.protocol();
+                if proto != 0x06 {
+                    // Not TCP
+                    continue;
+                }
+                match etherparse::TcpHeaderSlice::from_slice(&buf[4 + p.slice().len()..]) {
+                    Ok(t) => {
+                        eprintln!(
+                            "{} → {} {}b of tcp to port {}",
+                            src,
+                            dst,
+                            t.slice().len(),
+                            t.destination_port()
+                        );
+                    }
+                    Err(e) => {
+                        eprintln!("Ignoring TCP Packet {:?}", e);
+                    }
+                }
+            }
+            Err(e) => {
+                eprintln!("Ignoring packet {:?}", e);
+            }
+        }
     }
     Ok(())
 }
